27d322d520b470755502637e67389ea1675350ff
Konstantin Kruglov <kruglov.konstantin@outlook.com>
2023-02-24 23:35:32 +0400

    chore: add link icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,17 @@ export default defineNuxtConfig({
           name: 'description',
           content: 'Hands',
         },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' }
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      ],
     }
   },
 
