5ecfe5904a01fa015b86709a36e2303c14121413
Konstantin Kruglov <kruglov.konstantin@outlook.com>
2023-02-26 07:02:51 +0400

    fix: fix auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,14 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware((to, from) => {
   if (['/signup', '/signin'].includes(to.path)) {
     return
   }
 
-  const user = useSupabaseUser()
   const token = useSupabaseToken();
-  console.log(user, token)
-  // if (!token.value) {
-  //   return await navigateTo('/signin');
-  // }
+
+  if (!token.value) {
+    console.log('REDIRECT', token.value)
+    return navigateTo('/signin');
+  } else {
+    console.log('NOT REDIRECT', token.value)
+  }
 })
